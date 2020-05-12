1021eddb486ed01f3ab2ba9c453158098e4d45f8
Sachin Gokal <sachin.gokal@gmail.com>
2020-05-12 15:14:09 -0400

    use same random sample number for every subject

--- src/rf_helpers.py
+++ src/rf_helpers.py
@@ -9,8 +9,22 @@ from sklearn.metrics import (
 )
 from sklearn.model_selection import train_test_split, GridSearchCV
 
+POSSIBLE_COLUMNS = ['MEAN_RR', 'MEDIAN_RR', 'SDRR', 'RMSSD', 'SDSD', 'SDRR_RMSSD', 'HR',
+                    'pNN25', 'pNN50', 'SD1', 'SD2', 'KURT', 'SKEW', 'MEAN_REL_RR',
+                    'MEDIAN_REL_RR', 'SDRR_REL_RR', 'RMSSD_REL_RR', 'SDSD_REL_RR',
+                    'SDRR_RMSSD_REL_RR', 'KURT_REL_RR', 'SKEW_REL_RR', 'VLF', 'VLF_PCT',
+                    'LF', 'LF_PCT', 'LF_NU', 'HF', 'HF_PCT', 'HF_NU', 'TP', 'LF_HF',
+                    'HF_LF', 'sampen', 'higuci', 'MEAN_RR_LOG',
+                    'MEAN_RR_SQRT', 'TP_SQRT', 'MEDIAN_REL_RR_LOG', 'RMSSD_REL_RR_LOG',
+                    'SDSD_REL_RR_LOG', 'VLF_LOG', 'LF_LOG', 'HF_LOG', 'TP_LOG', 'LF_HF_LOG',
+                    'RMSSD_LOG', 'SDRR_RMSSD_LOG', 'pNN25_LOG', 'pNN50_LOG', 'SD1_LOG',
+                    'KURT_YEO_JONSON', 'SKEW_YEO_JONSON', 'MEAN_REL_RR_YEO_JONSON',
+                    'SKEW_REL_RR_YEO_JONSON', 'LF_BOXCOX', 'HF_BOXCOX', 'SD1_BOXCOX',
+                    'KURT_SQUARE', 'HR_SQRT', 'MEAN_RR_MEAN_MEAN_REL_RR', 'SD2_LF', 'HR_LF',
+                    'HR_HF', 'HF_VLF']
+
 COLUMNS_TO_KEEP = ['MEAN_RR', 'MEDIAN_RR', 'SDRR',
-                   'RMSSD', 'SDSD', 'SDRR_RMSSD', 'pNN25', 'pNN50',
+                   'RMSSD', 'RMSSD_LOG', 'SDSD', 'SDRR_RMSSD', 'pNN25', 'pNN50',
                    'HR']
 
 def rf_grid():
@@ -25,7 +39,7 @@ def rf_for_subject_subset(df, subject_threshold):
    X_train, X_test, y_train, y_test = train_test_split(X, y)
    clf = GridSearchCV(rf, rf_grid())
    clf.fit(X_train, y_train)
-   print(f'Accuracy Score for Subset: {accuracy_score(y_test, clf.predict(X_test))}')
+   print(f'Accuracy Score for Included Subset: {accuracy_score(y_test, clf.predict(X_test))}')
    return clf
 
 def test_rf_on_excluded_subset(clf, subject_threshold):
@@ -36,16 +50,25 @@ def test_rf_on_excluded_subset(clf, subject_threshold):
     print('Accuracy Score for Excluded Subset')
     return accuracy_score(y_test, clf.predict(X_test))
 
-def calibrate_rf_with_sample_of_excluded_subset(df, subject_threshold):
+def calibrate_rf_with_sample_of_excluded_subset(df, subject_threshold, samples_per_subject):
     subset_df = df[df['subject_id'] <= subject_threshold]
-    random_sample = df[df['subject_id'] > subject_threshold].sample(n=1000)
-    new_df = pd.concat([subset_df, random_sample])
-    calibrated_rf = rf_for_subject_subset(new_df, subject_threshold)
+    excluded_df = df[df['subject_id'] > subject_threshold]
+    copy_of_excluded_df = excluded_df.copy()
+    random_samples_idxs = []
 
-    # excluded test subset with random sample removed
-    excluded_df = df[df['subject_id'] > subject_threshold].drop(random_sample.index)
+    for i in copy_of_excluded_df['subject_id'].unique():
+        random_sample = df[df['subject_id'] == i].sample(n=samples_per_subject)
+        random_samples_idxs.append(random_sample.index)
+        subset_df = pd.concat([subset_df, random_sample])
+
+    calibrated_rf = rf_for_subject_subset(subset_df, subject_threshold)
+    # remove random sample from excluded df for test
+    print(f'TOTAL IDXS: {len(np.array(random_samples_idxs).flatten())}')
+    excluded_df = excluded_df.drop(np.array(random_samples_idxs).flatten())
+
     X_test = excluded_df[COLUMNS_TO_KEEP]
     X_test = StandardScaler().fit_transform(X_test)
     y_test = excluded_df['NasaTLX Label'].values
-    print('Accuracy Score for Excluded Subset')
-    return accuracy_score(y_test, calibrated_rf.predict(X_test))
+    print(
+        f'Accuracy Score for Excluded Subset: {accuracy_score(y_test, calibrated_rf.predict(X_test))}')
+    return calibrated_rf
