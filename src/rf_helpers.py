d8a14238065156c2d22305072a4174419061caee
Sachin Gokal <sachin.gokal@gmail.com>
2020-05-12 14:00:14 -0400

    add calibrate function

--- src/rf_helpers.py
+++ src/rf_helpers.py
@@ -17,24 +17,35 @@ def rf_grid():
     return {'n_estimators': [100, 250, 500], 'max_depth': [2, 3]}
 
 def rf_for_subject_subset(df, subject_threshold):
-   X = df[df['subject_id'] <= subject_threshold]
+   included_df = df[df['subject_id'] <= subject_threshold]
+   X = included_df
    X = X[COLUMNS_TO_KEEP]
    X = StandardScaler().fit_transform(X)
-   y = df[df['subject_id'] <= subject_threshold]['NasaTLX Label'].values
+   y = included_df['NasaTLX Label'].values
    X_train, X_test, y_train, y_test = train_test_split(X, y)
    clf = GridSearchCV(rf, rf_grid())
    clf.fit(X_train, y_train)
-   print(f'Accuracy Score: {accuracy_score(y_test, clf.predict(X_test))}')
+   print(f'Accuracy Score for Subset: {accuracy_score(y_test, clf.predict(X_test))}')
    return clf
 
 def test_rf_on_excluded_subset(clf, subject_threshold):
-    X_test = df[df['subject_id'] > subject_threshold][COLUMNS_TO_KEEP]
+    excluded_df = df[df['subject_id'] > subject_threshold]
+    X_test = excluded_df[COLUMNS_TO_KEEP]
     X_test = StandardScaler().fit_transform(X_test)
-    y_test = df[df['subject_id'] > subject_threshold]['NasaTLX Label'].values
+    y_test = excluded_df['NasaTLX Label'].values
+    print('Accuracy Score for Excluded Subset')
     return accuracy_score(y_test, clf.predict(X_test))
 
 def calibrate_rf_with_sample_of_excluded_subset(df, subject_threshold):
     subset_df = df[df['subject_id'] <= subject_threshold]
     random_sample = df[df['subject_id'] > subject_threshold].sample(n=1000)
     new_df = pd.concat([subset_df, random_sample])
-    return rf_for_subject_subset(new_df, subject_threshold)
+    calibrated_rf = rf_for_subject_subset(new_df, subject_threshold)
+
+    # excluded test subset with random sample removed
+    excluded_df = df[df['subject_id'] > subject_threshold].remove(random_sample.index)
+    X_test = excluded_df[COLUMNS_TO_KEEP]
+    X_test = StandardScaler().fit_transform(X_test)
+    y_test = excluded_df['NasaTLX Label'].values
+    print('Accuracy Score for Excluded Subset')
+    return accuracy_score(y_test, calibrated_rf.predict(X_test))
