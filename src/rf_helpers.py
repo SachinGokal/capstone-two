d19b572c462ab81d67f869a2601d1f488276829d
Sachin Gokal <sachin.gokal@gmail.com>
2020-05-13 00:16:33 -0400

    code fixes

--- src/rf_helpers.py
+++ src/rf_helpers.py
@@ -73,7 +73,8 @@ POSSIBLE_COLUMNS = ['HF',
                     'pNN25_LOG',
                     'pNN50',
                     'pNN50_LOG',
-                    'sampen']
+                    'sampen',
+                    'NasaTLX Label']
 
 COLUMNS_TO_KEEP = ['HF',
                    'HF_BOXCOX',
@@ -110,14 +111,15 @@ RF_GRID = {'n_estimators': [100, 250, 500, 1000], 'max_depth': [2, 3]}
 
 def random_included_and_excluded_df(df, total_excluded=5):
     random_subject_ids = np.random.choice(df['subject_id'].unique(), total_excluded)
-    excluded_df = df[df['subject_id'].isin(random_subject_ids)]
-    included_df = df[~df['subject_id'].isin(random_subject_ids)]
+    excluded_df = df[df['subject_id'].isin(
+        random_subject_ids)][POSSIBLE_COLUMNS]
+    included_df = df[~df['subject_id'].isin(
+        random_subject_ids)][POSSIBLE_COLUMNS]
     return included_df, excluded_df
 
 # NasaTLX label is low, medium, high which is the mapping done in the study
 def rf_for_subject_subset(included_df):
-   X = included_df
-   X = X[COLUMNS_TO_KEEP]
+   X = included_df.drop(columns='NasaTLX Label')
    X = StandardScaler().fit_transform(X)
    y = included_df['NasaTLX Label'].values
    X_train, X_test, y_train, y_test = train_test_split(X, y)
@@ -127,27 +129,27 @@ def rf_for_subject_subset(included_df):
    return clf
 
 def test_rf_on_excluded_subset(clf, excluded_df):
-    X_test = excluded_df[COLUMNS_TO_KEEP]
+    X_test = excluded_df.drop(columns='NasaTLX Label')
     X_test = StandardScaler().fit_transform(X_test)
     y_test = excluded_df['NasaTLX Label'].values
     print(f'Accuracy Score for Excluded Subset without Calibration: {accuracy_score(y_test, clf.predict(X_test))}')
 
 def combined_included_excluded_without_calibration(included_df, excluded_df):
     clf = rf_for_subject_subset(included_df)
-    test_rf_on_excluded_subset(excluded_df)
+    test_rf_on_excluded_subset(clf, excluded_df)
 
 # Default sample set to around 6.25% of data
 # This is not a great method to calibrate given sampled datapoints will be correlated with excluded datapoints
 # Ideally this should be done with fresh data points through a cycle of scenarios early in the prediction process
 def calibrated_rf_with_sample_of_excluded_subset(included_df, excluded_df, samples_per_subject=1000):
     for i in excluded_df['subject_id'].unique():
-        random_sample = df[df['subject_id'] == i].sample(n=samples_per_subject)
+        random_sample = excluded_df[excluded_df['subject_id'] == i].sample(n=samples_per_subject)
         included_df = pd.concat([included_df, random_sample])
         # remove random sample from excluded subset
         excluded_df.drop(random_sample.index, inplace=True)
 
     calibrated_rf = rf_for_subject_subset(included_df)
-    X_test = excluded_df[COLUMNS_TO_KEEP]
+    X_test = excluded_df.drop(columns='NasaTLX Label')
     X_test = StandardScaler().fit_transform(X_test)
     y_test = excluded_df['NasaTLX Label'].values
     print(
@@ -155,7 +157,7 @@ def calibrated_rf_with_sample_of_excluded_subset(included_df, excluded_df, sampl
     return calibrated_rf
 
 def test_on_validation_df(validation_df):
-    X_test = validation_df[COLUMNS_TO_KEEP]
+    X_test = validation_df[POSSIBLE_COLUMNS].drop(columns='NasaTLX Label')
     X_test = StandardScaler().fit_transform(X_test)
     y_test = validation_df['NasaTLX Label'].values
     print(f'Accuracy Score for Validation Subset without Calibration: {accuracy_score(y_test, clf.predict(X_test))}')
@@ -165,7 +167,7 @@ def rf_predictions_for_each_subject(df):
     for i in df['subject_id'].unique():
         subject_df = df[df['subject_id'] == i]
         X = subject_df
-        X = X[COLUMNS_TO_KEEP]
+        X = X[POSSIBLE_COLUMNS]
         X = StandardScaler().fit_transform(X)
         y = subject_df['NasaTLX Label'].values
         X_train, X_test, y_train, y_test = train_test_split(X, y)
